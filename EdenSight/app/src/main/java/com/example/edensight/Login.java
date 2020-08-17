1a0b536325d1542bf7635d004874681e7ae381f7
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2020-08-17 13:50:36 +0800

    Experimenting with HTTP connection with Accuweather API
    It works. Can retrieve data with API. Poggies.

--- EdenSight/app/src/main/java/com/example/edensight/Login.java
+++ EdenSight/app/src/main/java/com/example/edensight/Login.java
@@ -5,14 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Login extends AppCompatActivity {
 
+    TextView testTextView;
     EditText usernameInput, passwordInput;
     Button loginBtn;
 
@@ -21,6 +32,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        testTextView = findViewById(R.id.test_textView);
         usernameInput = findViewById(R.id.usernameInput);
         passwordInput = findViewById(R.id.passwordInput);
         loginBtn = findViewById(R.id.loginBtn);
@@ -29,6 +41,7 @@ public class Login extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
                 // Login Correct
                 if (usernameInput.getText().toString().equals("staff") && passwordInput.getText().toString().equals("staff")){
                     Toast toast = Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT);
@@ -42,6 +55,10 @@ public class Login extends AppCompatActivity {
                     Toast toast = Toast.makeText(getApplicationContext(),"Please provide the correct login details.", Toast.LENGTH_SHORT);
                     toast.show();
                 }
+
+                 */
+                // Testing HTTP Connection with API
+                new RetrieveLoginTask().execute();
             }
         });
     }
@@ -72,5 +89,48 @@ public class Login extends AppCompatActivity {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    // Internal class used to retrieve data
+    class RetrieveLoginTask extends AsyncTask<Void, Void, String>{
+        private Exception exception;
+
+        protected void onPreExecute(){
+            testTextView.setText("Not Sent Yet.");
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String urlText = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=n6D2aIqpQYd6trDQHw7qesXY1cppKgz5&q=kuching";
+
+            try {
+                URL url = new URL(urlText);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null){
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e){
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response){
+            if (response == null){
+                response = "THERE WAS AN ERROR";
+            }
+            // Cut to 50 characters
+            response = response.substring(0, Math.min(response.length(), 50));
+            testTextView.setText(response);
+        }
     }
 }
