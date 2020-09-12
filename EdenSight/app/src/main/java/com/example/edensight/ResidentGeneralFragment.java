690acec31001e32bc400f07e04b178d0ba95e860
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2020-09-12 18:22:18 +0800

    Resident Details page (Status Tab)

--- EdenSight/app/src/main/java/com/example/edensight/ResidentGeneralFragment.java
+++ EdenSight/app/src/main/java/com/example/edensight/ResidentGeneralFragment.java
@@ -7,12 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class ResidentGeneralFragment extends Fragment {
 
     private static final String GEN_RESIDENT = "Gen_Resident";
     private Resident resident;
+
+    LineChart lineChart;
 
     public ResidentGeneralFragment() { }
 
@@ -36,8 +45,33 @@ public class ResidentGeneralFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_resident_general, container, false);
-        TextView textView = rootView.findViewById(R.id.general_text);
-        textView.setText("Name: " + resident.getName());
+
+        lineChart = rootView.findViewById(R.id.general_graph);
+        ArrayList<String> xAxis = new ArrayList<>();
+        ArrayList<Entry> yAxis = new ArrayList<>();
+
+        float x = 0, y = 0;
+        int numDataPoints = 5;
+
+        for (int i=0; i < numDataPoints; i++){
+            y = y + 1;
+            x = x + 1;
+            yAxis.add(new Entry(y, i));
+            xAxis.add(i, String.valueOf(x));
+        }
+        String[] xAxes = new String[xAxis.size()];
+        for (int i = 0; i < xAxis.size(); i++){
+            xAxes[i] = xAxis.get(i);
+        }
+
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+        LineDataSet lineDataSet1 = new LineDataSet(yAxis, "y");
+        lineDataSet1.setDrawCircles(false);
+
+        lineDataSets.add(lineDataSet1);
+        lineChart.setData(new LineData(lineDataSets));
+        lineChart.setVisibleXRangeMaximum(65f);
+
         return rootView;
     }
 }
