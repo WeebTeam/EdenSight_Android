beb40cf21af1e759b089708174b8b6f315c7f3cb
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2020-09-13 15:34:03 +0800

    Resident Details Stuff (WIP 2)

--- EdenSight/app/src/main/java/com/example/edensight/ResidentDetailedFragment.java
+++ EdenSight/app/src/main/java/com/example/edensight/ResidentDetailedFragment.java
@@ -13,6 +13,7 @@ public class ResidentDetailedFragment extends Fragment {
 
     private static final String DET_RESIDENT = "Det_Resident";
     private Resident resident;
+    TextView name, caretaker, roomNo, gender, dob, ic, nation, phoneNo, guardian, emergency, address;
 
     public ResidentDetailedFragment() { }
 
@@ -36,8 +37,23 @@ public class ResidentDetailedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_resident_detailed, container,false);
-        TextView textView = rootView.findViewById(R.id.detailed_text);
-        textView.setText("Allocated Date: " + resident.getAllocationDate());
+        name = rootView.findViewById(R.id.resident_detailed_name);
+        caretaker = rootView.findViewById(R.id.resident_detailed_caretaker);
+        roomNo = rootView.findViewById(R.id.resident_detailed_room_number);
+        gender = rootView.findViewById(R.id.resident_detailed_gender);
+        dob = rootView.findViewById(R.id.resident_detailed_dob);
+        ic = rootView.findViewById(R.id.resident_detailed_ic);
+        nation = rootView.findViewById(R.id.resident_detailed_nation);
+        phoneNo = rootView.findViewById(R.id.resident_detailed_phone_number);
+        guardian = rootView.findViewById(R.id.resident_detailed_guardian);
+        emergency = rootView.findViewById(R.id.resident_detailed_emergency);
+        address = rootView.findViewById(R.id.resident_detailed_address);
+
+        // More will be added once database has all values
+        name.setText(": " + resident.getName());
+        caretaker.setText(": " + resident.getCaretaker());
+        roomNo.setText(": " + resident.getRoomNumber());
+
         return rootView;
     }
 }
--- EdenSight/app/src/main/java/com/example/edensight/ResidentHealthFragment.java
+++ EdenSight/app/src/main/java/com/example/edensight/ResidentHealthFragment.java
@@ -7,12 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ResidentHealthFragment extends Fragment {
 
     private static final String HEL_RESIDENT = "Hel_Resident";
     private Resident resident;
+
+    TextView weight, height, blood;
+    ListView conditions, allergies, medication;
+
+    String[] conditionsArray = {"Back Problem","Asthma", "Migraine", "Back Problem","Asthma", "Migraine"},
+    allergiesArray = {"Penicillin", "Aspirin", "Peanuts"},
+    medicationArray = {"Painkiller once a day", "Flu Medicine 3 times a day", "Medicine X after breakfast", "Another one", "And another one"};
+
+    ArrayAdapter conditionsAdapter, allergiesAdapter, medicationAdapter;
 
     public ResidentHealthFragment() { }
 
@@ -36,8 +47,22 @@ public class ResidentHealthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_resident_health, container,false);
-        TextView textView = rootView.findViewById(R.id.health_text);
-        textView.setText("Caretaker: " + resident.getCaretaker());
+        weight = rootView.findViewById(R.id.health_weight);
+        height = rootView.findViewById(R.id.health_height);
+        blood = rootView.findViewById(R.id.health_blood);
+        conditions = rootView.findViewById(R.id.health_conditions);
+        allergies = rootView.findViewById(R.id.health_allergies);
+        medication = rootView.findViewById(R.id.health_medication);
+
+        // Assign values to text here when values are available in database
+        conditionsAdapter = new ArrayAdapter<String>(getActivity(), R.layout.listview, conditionsArray);
+        allergiesAdapter = new ArrayAdapter<String>(getActivity(), R.layout.listview, allergiesArray);
+        medicationAdapter = new ArrayAdapter<String>(getActivity(), R.layout.listview, medicationArray);
+
+        conditions.setAdapter(conditionsAdapter);
+        allergies.setAdapter(allergiesAdapter);
+        medication.setAdapter(medicationAdapter);
+
         return rootView;
     }
 }
